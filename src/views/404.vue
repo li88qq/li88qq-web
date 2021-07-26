50f2ba0701e3b725074fe35b1798ccdf8b93e92a
li88qq <573258011@qq.com>
2021-07-26 23:27:34 +0800

    增加404页面.

---
+++ src/views/404.vue
@@ -0,0 +1,21 @@
+<template>
+  <a-result status="404" title="404" sub-title="Sorry, the page you visited does not exist.">
+  </a-result>
+</template>
+
+<script>
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: '',
+  components: {},
+  emits: [],
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
