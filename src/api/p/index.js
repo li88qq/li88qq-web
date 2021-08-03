7394b6ebebc73e38ec1d5fe36f9d202532fd6acc
li88qq <573258011@qq.com>
2021-08-03 23:04:31 +0800

    增加应用时间戳功能.

--- src/api/p/index.js
+++ src/api/p/index.js
@@ -2,6 +2,8 @@ import vAxios from '/@/utils/axiosUtil'
 
 const Api = {
     CAPTCHA: '/p/getCaptcha',
+    IP: '/p/ip',
 }
 
 export const getCaptcha = () => vAxios.get(Api.CAPTCHA, {responseType: 'blob'})
+export const getIp = () => vAxios.get(Api.IP)
