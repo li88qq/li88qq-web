3bf51cfd0be012442c420b5e03645a5cb0449b15
li88qq <573258011@qq.com>
2021-08-03 22:12:50 +0800

    时间工具类新增多个方法.

--- src/utils/dateUtil.js
+++ src/utils/dateUtil.js
@@ -1,8 +1,14 @@
 import moment from 'moment';
 
 const DATE_TIME_FORMAT = 'YYYY-MM-DD HH:mm';
+const DATE_SECOND_FORMAT = 'YYYY-MM-DD HH:mm:ss';
 const DATE_FORMAT = 'YYYY-MM-DD';
 
+/**
+ * 时间戳格式化日期
+ * @param date
+ * @returns {string}
+ */
 export function timestampToDate(date) {
     if (!date || date <= 0) {
         return '';
@@ -10,22 +16,106 @@ export function timestampToDate(date) {
     return moment.unix(date).format(DATE_FORMAT);
 }
 
-export function timestampToTime(date) {
+/**
+ * 时间戳格式化时间
+ * @param date
+ * @param second 是否显示秒
+ * @returns {string}
+ */
+export function timestampToTime(date, second) {
     if (!date || date <= 0) {
         return '';
     }
-    return moment.unix(date).format(DATE_TIME_FORMAT);
+    if (!second) {
+        return moment.unix(date).format(DATE_TIME_FORMAT);
+    } else {
+        return moment.unix(date).format(DATE_SECOND_FORMAT);
+    }
 }
 
+/**
+ * 当前日期格式化日期
+ * @returns {string}
+ */
 export function today() {
     return moment().format(DATE_FORMAT);
 }
 
+/**
+ * moment格式化日期
+ * @param momentObj
+ * @returns {null|*}
+ */
 export function toDate(momentObj) {
-    if(!momentObj){
+    if (!momentObj) {
         return null;
     }
     return momentObj.format(DATE_FORMAT);
+}
+
+/**
+ * moment转时间戳秒
+ * @param momentObj
+ * @returns {null|*}
+ */
+export function momentToSecond(momentObj) {
+    if (!momentObj) {
+        return null;
+    }
+    return momentObj.unix();
+}
+
+/**
+ * moment转时间戳毫秒
+ * @param momentObj
+ * @returns {Object|null}
+ */
+export function momentToMs(momentObj) {
+    if (!momentObj) {
+        return null;
+    }
+    return momentObj.valueOf();
+}
+
+/**
+ * 时间戳秒转moment
+ * @param timestamp
+ */
+export function timestampToMoment(timestamp) {
+    if (!timestamp) {
+        return null;
+    }
+    return moment.unix(timestamp);
+}
+
+/**
+ * 时间戳毫秒转moment
+ * @param timestamp
+ */
+export function msToMoment(timestamp) {
+    if (!timestamp) {
+        return null;
+    }
+    return moment(timestamp);
+}
+
+/**
+ * 获取当前moment
+ * @returns {moment.Moment}
+ */
+export function momentNow() {
+    return moment();
+}
+
+/**
+ * moment格式化
+ * @param momentObj
+ */
+export function momentFormat(momentObj) {
+    if (!momentObj) {
+        return null;
+    }
+    return momentObj.format(DATE_SECOND_FORMAT);
 }
 
 export const dateUtil = moment;
