af50e0b4eafec22bc3bf85a4b601ff28fc679e8b
li88qq <573258011@qq.com>
2021-08-04 23:37:25 +0800

    加入登出接口.

--- src/api/login/index.js
+++ src/api/login/index.js
@@ -2,6 +2,8 @@ import vAxios from '/@/utils/axiosUtil'
 
 const Api = {
     LOGIN: '/login',
+    LOGOUT:'/logout',
 }
 
 export const login = (params) => vAxios.post(Api.LOGIN, params)
+export const logout = (params) => vAxios.post(Api.LOGOUT, params)
