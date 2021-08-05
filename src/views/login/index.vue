edec5a4abb3f9c88f38df27e94bc4c762f63b7bb
li88qq <573258011@qq.com>
2021-08-05 23:21:34 +0800

    登录页面调整.

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,24 +1,38 @@
 <template>
   <div class="container">
-    <a-card title="欢迎登录li88qq平台" class="login-form">
-      <a-form :model="formRt" :label-col="{span:4}" :wrapper-col="{span:16}">
-        <a-form-item label="用户名" name="username">
-          <a-input v-model:value="formRt.username"/>
-        </a-form-item>
-        <a-form-item label="密码" name="password">
-          <a-input-password v-model:value="formRt.password"/>
-        </a-form-item>
-        <a-form-item label="验证码" name="code">
-          <div style="display: flex;flex-direction: row;">
-            <a-input v-model:value="formRt.code" style="flex:1;"/>
-            <img :src="codeRef" @click="changeAc" style="width: 160px;"/>
-          </div>
-        </a-form-item>
-        <a-form-item :wrapper-col="{span:16,offset:4}">
-          <a-button type="primary" block @click="loginAc">登录</a-button>
-        </a-form-item>
-      </a-form>
-    </a-card>
+    <div class="login-container">
+      <a-card :title="'欢迎登录'+envRt.name+'平台'" class="login-form">
+        <a-form ref="formRef" :model="formRt" :label-col="{span:4}" :wrapper-col="{span:16}" :rules="rules">
+          <a-form-item label="用户名" name="username">
+            <a-input v-model:value="formRt.username"/>
+          </a-form-item>
+          <a-form-item label="密码" name="password">
+            <a-input-password v-model:value="formRt.password"/>
+          </a-form-item>
+          <a-form-item label="验证码" name="code">
+            <div style="display: flex;flex-direction: row;">
+              <a-input v-model:value="formRt.code" style="flex:1;" @keyup.enter="loginAc"/>
+              <img :src="codeRef" @click="changeAc" style="width: 120px;height: 32px;"/>
+            </div>
+          </a-form-item>
+          <a-form-item :wrapper-col="{span:16,offset:4}">
+            <a-button type="primary" block @click="loginAc">登录</a-button>
+          </a-form-item>
+        </a-form>
+      </a-card>
+    </div>
+    <div class="footer">
+      <div class="footer-header">
+        <a target="_blank" :href="envRt.url" class="footer-header-a">
+          <img src="/beian.png">
+          <p>{{ envRt.url1 }}</p>
+        </a>
+        <a href="https://beian.miit.gov.cn/" target="_blank">
+          <p>{{ envRt.url2 }}</p>
+        </a>
+      </div>
+      <div><p>{{envRt.copyRight}}</p></div>
+    </div>
   </div>
 </template>
 
@@ -33,10 +47,18 @@ export default defineComponent({
   props: {},
   components: {},
   setup() {
+    const formRef = ref()
     const formRt = reactive({
-      username: '',
-      password: '',
+      username: 'test',
+      password: '123456',
       code: ''
+    })
+    const envRt = reactive({
+      url: '',
+      url1: '',
+      url2: '',
+      name: '',
+      copyRight: '',
     })
 
     const codeRef = ref('')
@@ -44,8 +66,9 @@ export default defineComponent({
 
     const loginAc = async () => {
       try {
-        await login(toRaw(formRt))
-        replace('/home').catch((()=>{}))
+        const data = await formRef.value.validate();
+        await login(data)
+        replace('/my/view').catch(()=>{})
       } catch (err) {
 
       }
@@ -59,10 +82,41 @@ export default defineComponent({
       codeRef.value = URL.createObjectURL(blob);
     }
 
+    const rules = {
+      username: [
+        {required: true, message: '请输入用户名', trigger: 'blur'},
+        {min: 2, max: 12, message: '用户名2-12个字符', trigger: 'blur'},
+      ],
+      password: [
+        {required: true, message: '请输入密码', trigger: 'blur'},
+        {min: 6, max: 12, message: '密码6-12个字符', trigger: 'blur'},
+      ],
+      code: [
+        {required: true, message: '请输入验证码', trigger: 'blur'},
+        {min: 4, max: 4, message: '验证码4个字符', trigger: 'blur'},
+      ],
+    }
+
+    const init = () => {
+      const {
+        VITE_APP_BENAN_URL,
+        VITE_APP_BENAN_1,
+        VITE_APP_BENAN_2,
+        VITE_APP_NAME,
+        VITE_APP_COPYRIGHT
+      } = import.meta.env
+      envRt.url = VITE_APP_BENAN_URL
+      envRt.url1 = VITE_APP_BENAN_1
+      envRt.url2 = VITE_APP_BENAN_2
+      envRt.name = VITE_APP_NAME
+      envRt.copyRight = VITE_APP_COPYRIGHT
+    }
+
     onMounted(() => {
+      init()
       queryCode()
     })
-    return {formRt, codeRef, loginAc, changeAc}
+    return {formRef, formRt, codeRef, envRt, rules, loginAc, changeAc}
   }
 })
 </script>
@@ -76,7 +130,40 @@ export default defineComponent({
   align-items: center;
 }
 
+.login-container {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
 .login-form {
   width: 600px;
+}
+
+.footer {
+  background-color: #2e3033;
+  width: 100%;
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.footer p {
+  margin: 0;
+  color: #fff;
+}
+
+.footer-header {
+  display: flex;
+  flex-direction: row;
+}
+
+.footer-header-a {
+  display: flex;
+  flex-direction: row;
+  align-items: end;
+  margin-right: 10px;
 }
 </style>
