dc9cb6ccd7ce28b0ec75a4ac5f75fff70d739cce
li88qq <573258011@qq.com>
2021-08-15 12:15:15 +0800

    增加我的看板管理页面.

---
+++ src/views/my/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout>
+    <router-view></router-view>
+  </Layout>
+</template>
+
+<script>
+import {defineComponent} from 'vue';
+import Layout from '/@/layouts/index.vue'
+
+export default defineComponent({
+  name: '',
+  props: {Layout},
+  components: {},
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
