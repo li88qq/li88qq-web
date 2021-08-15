82851770dbc0a2feec64d4d965544049b60ca64a
li88qq <573258011@qq.com>
2021-08-15 12:12:27 +0800

    调整公共api接口.

--- src/api/p/index.js
+++ src/api/p/index.js
@@ -3,7 +3,11 @@ import vAxios from '/@/utils/axiosUtil'
 const Api = {
     CAPTCHA: '/p/getCaptcha',
     IP: '/p/ip',
+    ARTICLE: '/p/article',
+    ARTICLE_READ: '/p/readArticle',
 }
 
 export const getCaptcha = () => vAxios.get(Api.CAPTCHA, {responseType: 'blob'})
 export const getIp = () => vAxios.get(Api.IP)
+export const getArticle = (params) => vAxios.get(Api.ARTICLE, {params: params})
+export const readArticle = (params) => vAxios.post(Api.ARTICLE_READ, params, {hideMsg: true});
