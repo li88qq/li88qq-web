586659fb5edd413156b4311be574087bd159f95c
li88qq <573258011@qq.com>
2021-10-10 19:33:33 +0800

    调整短信发送接口参数.

--- src/api/p/index.js
+++ src/api/p/index.js
@@ -12,4 +12,4 @@ export const getCaptcha = () => vAxios.get(Api.CAPTCHA, {responseType: 'blob'})
 export const getIp = () => vAxios.get(Api.IP)
 export const getArticle = (params) => vAxios.get(Api.ARTICLE, {params: params})
 export const readArticle = (params) => vAxios.post(Api.ARTICLE_READ, params, {hideMsg: true});
-export const smsCode = (params) => vAxios.post(Api.SMS_CODE, params, {hideMsg: true});
+export const smsCode = (params) => vAxios.post(Api.SMS_CODE, params);
