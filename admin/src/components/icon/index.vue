a0d1fc1c94a09548246dcbca52e4116e86e6767e
li88qq <573258011@qq.com>
2022-03-29 23:55:26 +0800

    [admin]:增加Icon组件.

---
+++ admin/src/components/icon/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <Icon :icon="icon" :class="compClass"></Icon>
+</template>
+
+<script lang="ts">
+import {computed, defineComponent, toRaw} from 'vue'
+import {Icon} from "@iconify/vue";
+
+export default defineComponent({
+  name: 'Icon',
+  props: {icon: {}, btn: {}},
+  components: {Icon},
+  emits: [],
+  setup(props) {
+    const compClass = computed(() => {
+      if (toRaw(props.btn)) {
+        return 'anticon';
+      }
+    })
+    return {compClass}
+  }
+})
+</script>
