73cd47b56b670b32e72b7b1d05ade6f015859ada
li88qq <573258011@qq.com>
2023-08-22 23:44:31 +0800

    [main-v2]:加入VueDraggable

--- main-v2/src/views/app/navigation/index.vue
+++ main-v2/src/views/app/navigation/index.vue
@@ -4,6 +4,16 @@
       <div>
         <a-input/>
         <a-tree></a-tree>
+        <VueDraggable
+            v-model="dataRef"
+            item-key="id"
+            :animation="200"
+            ghost-class="draggable-ghost"
+        >
+          <template #item="{element}">
+            <div class="draggable-item">{{element.id}}</div>
+          </template>
+        </VueDraggable>
       </div>
     </template>
     <template #body>
@@ -12,8 +22,32 @@
   </PageView>
 </template>
 <script setup lang="ts">
-import {ref,reactive} from 'vue'
+import {ref,reactive,onMounted} from 'vue'
 import {PageView} from '@/components/pageview'
 import {BasicTable} from '@/components/table'
+import VueDraggable from 'vuedraggable'
+
+const dataRef = ref([])
+
+onMounted(()=>{
+  for(let i = 0;i<10;i++){
+    dataRef.value.push({id:i});
+  }
+})
 
 </script>
+<style scoped>
+.draggable-item{
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #eee;
+  padding: 8px;
+  margin: 8px;
+}
+.draggable-ghost{
+  opacity: 0.5;
+  background-color: #eee;
+}
+</style>
