bda91e5ab36d5a1d01c1914a6ed7ed716fff59a5
li88qq <57325800@qq.com>
2024-05-26 23:29:28 +0800

    增加依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {join} from 'node:path'
 
-export default defineConfig(()=>{
+export default defineConfig(({mode})=>{
+    const env = loadEnv(mode,'.')
+
     return{
         server:{
             host:true,
+            proxy:{
+                '/apis':{
+                    target:env.VITE_APIS_HOST,
+                    changeOrigin:true,
+                    rewrite: (path) => path.replace(/^\/apis/, '')
+                }
+            }
         },
         plugins:[vue()],
         resolve:{
             alias:{
                 '@':join(__dirname,'./src')
             }
-        }
+        },
     }
 })
